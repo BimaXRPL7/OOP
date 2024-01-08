513b44541ac20323df104f36f2897b3b1c3734ab
Bima <141801856+BimaXRPL7@users.noreply.github.com>
2024-01-08 08:28:12 +0700

    Add files via upload

---
+++ Sekolah.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Sekolah{
+    Scanner input = new Scanner(System.in);
+    public void tampilNama(String nama){
+        System.out.println("Nama Sekolah: "+nama);
+    }
+
+    public void jumlahSiswa(int rombel, int jumlahSiswa){
+        int total=rombel*jumlahSiswa;
+        System.out.println("Jumlah Siswa: "+total);
+    }
+}
